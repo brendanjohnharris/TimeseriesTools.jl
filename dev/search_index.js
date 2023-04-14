82f753d0f3aa8e266647addbba024fd7a95ca4d7
Documenter.jl <documenter@juliadocs.github.io>
2023-04-14 17:41:21 +0000

    build based on 65b468e

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TimeseriesTools","category":"page"},{"location":"#TimeseriesTools","page":"Home","title":"TimeseriesTools","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TimeseriesTools.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TimeseriesTools]","category":"page"}]
+}
