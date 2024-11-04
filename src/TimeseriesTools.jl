25101d5773e1b99a1130842a4ceb0f3a235e0797
brendanjohnharris <bhar9988@uni.sydney.edu.au>
2024-11-04 17:25:03 +1100

    Remove Requires as a dependency

--- src/TimeseriesTools.jl
+++ src/TimeseriesTools.jl
@@ -14,17 +14,6 @@ using Requires
 
 function __init__()
     ENV["UNITFUL_FANCY_EXPONENTS"] = true
-    # @static if !isdefined(Base, :get_extension)
-    # @require Makie="ee78f7c6-11fb-53f2-987a-cfe4a2b5a57a" begin
-    #     @eval include("../ext/MakieExt.jl")
-    # end
-    # @require DSP="717857b8-e6f2-59f4-9121-6e50c889abd2" begin
-    #     @eval include("../ext/DSPExt.jl")
-    # end
-    # @require TimeseriesSurrogates="c804724b-8c18-5caa-8579-6025a0767c70" begin
-    #     @eval include("../ext/TimeseriesSurrogatesExt.jl")
-    # end
-    # end
 end
 
 include("Types.jl")
